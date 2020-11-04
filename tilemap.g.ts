6d1e5e2ac00f4e53b1956d1c6da09897de2e1344
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2020-11-04 09:51:59 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -74,6 +74,8 @@ namespace myTiles {
     export const tile35 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile36 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile37 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
